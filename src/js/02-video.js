7497d87464b5578f0b598bf50c192f33d79d11ab
Samoylenko Dmitry <107771156+dmytrik@users.noreply.github.com>
2022-07-07 19:18:12 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(saveSecondsInStorage, 1000));
+
+function saveSecondsInStorage(event) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(event.seconds)
+  );
+}
+
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
